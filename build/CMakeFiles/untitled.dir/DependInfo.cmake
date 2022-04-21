6ddfc13634d1fa41f8a43684d169cb6f86b54db5
Ladbaby <1390265970@qq.com>
2022-04-21 19:51:54 +0800

    0421

---
+++ build/CMakeFiles/untitled.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Firefly/Apps/E/VS_Code/VS_Code_C++/Qt/untitled/battery.cpp" "CMakeFiles/untitled.dir/battery.cpp.obj" "gcc" "CMakeFiles/untitled.dir/battery.cpp.obj.d"
+  "C:/Users/Firefly/Apps/E/VS_Code/VS_Code_C++/Qt/untitled/frmbattery.cpp" "CMakeFiles/untitled.dir/frmbattery.cpp.obj" "gcc" "CMakeFiles/untitled.dir/frmbattery.cpp.obj.d"
+  "C:/Users/Firefly/Apps/E/VS_Code/VS_Code_C++/Qt/untitled/main.cpp" "CMakeFiles/untitled.dir/main.cpp.obj" "gcc" "CMakeFiles/untitled.dir/main.cpp.obj.d"
+  "C:/Users/Firefly/Apps/E/VS_Code/VS_Code_C++/Qt/untitled/mainwindow.cpp" "CMakeFiles/untitled.dir/mainwindow.cpp.obj" "gcc" "CMakeFiles/untitled.dir/mainwindow.cpp.obj.d"
+  "C:/Users/Firefly/Apps/E/VS_Code/VS_Code_C++/Qt/untitled/build/untitled_autogen/mocs_compilation.cpp" "CMakeFiles/untitled.dir/untitled_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/untitled.dir/untitled_autogen/mocs_compilation.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
